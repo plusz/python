3d7f569c19c3a7be20841b4a41c06dfb4206c348
plusz <Konto@Github410>
2018-09-12 20:08:53 +0200

    log messages

--- parser.py
+++ parser.py
@@ -2,7 +2,10 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+print('opening data file')
 r = requests.get("http://www.slownikdlagier.pl/slowa-dopuszczalne-w-scrabble/ko/")
+print('parsing data file')
 soup = BeautifulSoup(r.content, "html.parser")
+print('filtering data')
 for a in sorted(soup.find_all('a', text=re.compile('^k[o|ó][a-z]{3,5}$')), key=lambda x: x.len):
     print(a.text)
